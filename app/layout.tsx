67a1bf4abf6ac5ecffadbd8ca1cb2c398368d1bd
brilliantsunlight1109 <brilliantsunlight1109@gmail.com>
2024-07-19 08:36:22 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
